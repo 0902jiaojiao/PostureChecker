fb0edea86e8335e6e636f012c91190644a159661
jiaojiao <0902jiaojiao@gmail.com>
2023-07-17 11:41:59 +0800

    改进了角度监测

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ mp_pose = mp.solutions.pose
 cap = cv2.VideoCapture(0)
 
 # 头部倾斜角度阈值 
-ANGLE_THRESHOLD = 20 
+ANGLE_THRESHOLD = 180
 
 
 def calculate_angle(a,b,c):
     radians = np.arctan2(c.y-b.y, c.x-b.x) - np.arctan2(a.y-b.y, a.x-b.x)
-    angle = np.abs(radians*180.0/np.pi)
+    angle = radians*180.0/np.pi
+    if angle < 0:
+        angle = 360+angle
     return angle
 
 
@@ -49,8 +51,11 @@ with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as 
             right_eye_outer = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_EYE_OUTER]
             right_eye_inner = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_EYE_INNER]
             
-            right_angle = calculate_angle(right_ear, right_eye_outer, right_eye_inner)
+            right_angle = calculate_angle(right_eye_inner, right_eye_outer, right_ear)
             cv2.putText(image, f'right: {right_angle}', (20,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+
+            if left_angle > ANGLE_THRESHOLD or right_angle > ANGLE_THRESHOLD:
+                cv2.putText(image, 'raise up your head!', (20,150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
 
         cv2.imshow('Pose Estimation', image)
 
